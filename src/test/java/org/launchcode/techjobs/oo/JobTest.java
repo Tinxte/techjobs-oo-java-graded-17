fb8301aad9ae8be8bbca50a34600ff20f521eb76
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-04-29 22:48:03 -0500

    JobId test

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,6 +1,21 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    //test empty constructor
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    //TODO: test full constructor
 
 }
